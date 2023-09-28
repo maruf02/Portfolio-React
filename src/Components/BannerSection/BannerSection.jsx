ceee2c2b9638b30fe4b1ec879f5b1f53a5fe89cd
maruf02 <rashiduzzaman99@gmail.com>
2023-09-28 17:02:55 +0600

    header/banner Complete

--- src/Components/BannerSection/BannerSection.jsx
+++ src/Components/BannerSection/BannerSection.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BannerIntro from "../BannerIntro/BannerIntro";
 import ProPicAnime from "../ProPicAnime/ProPicAnime";
 import BannerClient from "../BannerClient/BannerClient";
 
 const BannerSection = () => {
+  useEffect(() => {
+    window.onload = function () {
+      Particles.init({
+        selector: ".background",
+        maxParticles: 150,
+        connectParticles: "true",
+        speed: 1,
+        minDistance: 40,
+        sizeVariations: 8,
+        color: "#87c3fa",
+      });
+    };
+  }, []);
   return (
     <div>
+      <canvas
+        className="background absolute block top-0 left-0 z-0"
+        id="particles-js"
+      ></canvas>
       <div className="bg-gradient-to-r from-[#114250] to-[#131514]  bg-no-repeat font-f py-16 ">
         <section className="container mx-auto lg:pt-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:mx-auto  ">
           {/* <!-- left side --> */}
